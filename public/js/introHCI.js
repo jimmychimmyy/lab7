6c340af34b34fd23bd5268d71d0792c14d985491
Jim Chen <jjc041@ucsd.edu>
2019-02-21 01:50:11 -0800

    initial commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(clicked);
+}
+
+function clicked() {
+	ga("send", "event", "like", "click");
 }
